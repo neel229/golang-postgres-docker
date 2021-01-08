8fc4d8a6735137cda02eba7ced5bdfa19032dae0
neel229 <device.neel@gmail.com>
2021-01-08 23:48:17 +0530

    main_test.go added

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:postgres@172.17.212.235:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to the Database", err)
+	}
+	testQueries = New(conn)
+	os.Exit(m.Run())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/neel229/golang-postgres-docker
 
 go 1.15
+
+require github.com/lib/pq v1.9.0
