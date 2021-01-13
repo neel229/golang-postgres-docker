254e977360cdb8e95901082bb43350bdd66161e0
neel229 <device.neel@gmail.com>
2021-01-13 15:00:24 +0530

    main_test.go updated

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,19 +7,18 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:postgres@localhost:5432/simple_bank?sslmode=disable"
+	"github.com/neel229/golang-postgres-docker/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot open the config file:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to the Database", err)
 	}
