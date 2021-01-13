10eca8dfe7a1a115f2516b9f2f9b833994aeefab
neel229 <device.neel@gmail.com>
2021-01-13 14:57:35 +0530

    env config file added

--- main.go
+++ main.go
@@ -6,16 +6,15 @@ import (
 
 	"github.com/neel229/golang-postgres-docker/api"
 	db "github.com/neel229/golang-postgres-docker/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:postgres@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/neel229/golang-postgres-docker/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config file: ", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to the Database", err)
 	}
@@ -23,7 +22,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.StartServer(serverAddress)
+	err = server.StartServer(config.ServerAddress)
 	if err != nil {
 		log.Fatalf("Error starting the server: %v", err)
 	}
